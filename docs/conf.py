36090d000b1f1df96a9079241b19193c35ce2e8c
Philippe Miron <philippemiron@gmail.com>
2024-06-27 23:49:56 -0400

    linting

--- docs/conf.py
+++ docs/conf.py
@@ -9,13 +9,11 @@ sys.path.insert(0, os.path.abspath("../src"))
 
 # -- Project information
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = "Alice RAG"
 copyright = "2024"
 author = "Philippe Miron"
 
 # -- General configuration
-
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.mathjax",
@@ -33,7 +31,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output
-
 html_theme = "sphinx_book_theme"
 html_logo = "logo.webp"
 html_favicon = "favicon.ico"
